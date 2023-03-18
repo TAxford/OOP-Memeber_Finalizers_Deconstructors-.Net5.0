b76079e3474b54d7c85ed7415dff887f18329376
Timothy Axford <t.axford2@yahoo.com>
2023-03-18 10:36:03 -0400

    Add project files.

---
+++ OOP Memeber_Finalizers_Deconstructors .Net5.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Memeber_Finalizers_Deconstructors_.Net5._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
