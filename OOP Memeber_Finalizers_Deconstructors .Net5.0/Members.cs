f0cec321f80b43ff0e8660fa2c42236b838c0c93
Timothy Axford <t.axford2@yahoo.com>
2023-03-18 10:42:18 -0400

    Added a public member field example

--- OOP Memeber_Finalizers_Deconstructors .Net5.0/Members.cs
+++ OOP Memeber_Finalizers_Deconstructors .Net5.0/Members.cs
@@ -11,5 +11,8 @@ namespace OOP_Memeber_Finalizers_Deconstructors_.Net5._0
         // member - private field
         private string memberName;
         private string jobTitle;
+
+        // member - public field
+        public int age;
     }
 }
